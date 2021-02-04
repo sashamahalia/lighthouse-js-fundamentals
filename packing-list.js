35bb3a46297758c32d3d331351e2be8bca6cbb9f
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-02-04 22:24:20 +0000

    loop to print out a list of items to pack

---
+++ packing-list.js
@@ -0,0 +1,14 @@
+let packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+/*console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}*/
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  let item = packingList[i];
+  packingList = item;
+}
+
+console.log(packingList);
